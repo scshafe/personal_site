2af5cf7fe2c78d54c589400a01abe93096c4cc60
coleshaffer <coleshffr@gmail.com>
2020-12-19 11:57:02 -0800

    added basic html/js/css for experience and skill section:

---
+++ app/javascript/packs/skills.js
@@ -0,0 +1,8 @@
+
+
+$(function () {
+  $('.skill-item').click(function() {
+    let description_id = this.id.replace('-button', '-description');
+    $('#'+description_id).toggleClass('skill-description-hidden skill-description-visible');
+  })
+})
